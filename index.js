6c8e75eb374d88355ca9e852d126c32ab8046c34
neel123go <ayounpaul1029@gmail.com>
2022-04-23 17:02:29 +0600

    completed full CURD method

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bqm81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,6 +25,47 @@ async function run() {
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         });
+
+        // Get All Products
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // Delete A Single Product
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // Get Single Product
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+
+        // Update Single Product
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const options = { upsert: true };
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    price: updatedUser.price,
+                    img: updatedUser.img
+                }
+            };
+            const result = await productCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
     } finally {
 
     }
